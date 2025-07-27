10628b5aa0b7c69960d36154a373abadb217f75d
Dang <vodang.work@gmail.com>
2025-07-27 13:55:45 +0700

    Update index.js

--- anime-backend/index.js
+++ anime-backend/index.js
@@ -36,7 +36,7 @@ const server = http.createServer(app);
 // // ✅ Khởi tạo socket server
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://anime-info-webiste.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true, // Nếu dùng auth socket
   },
